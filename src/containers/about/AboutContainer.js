15c4d9088d3de492c0aaa6ea0bafa61a09c1e74b
Geoffrey <geoffrey.leborges@epitech.eu>
2018-02-16 14:26:42 +0100

    postcss-loader

--- src/containers/about/AboutContainer.js
+++ src/containers/about/AboutContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class About extends Component {
+export default class AboutContainer extends Component {
     render() {
         return (
             <div>
